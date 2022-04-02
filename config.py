13cc402eb737723abbd539923d139fcaeb5e3d04
fabrikart1 <99741038+fabrikart1@users.noreply.github.com>
2022-04-02 19:45:17 +0300

    Игра "Угадай число"

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+telegram_token = '5130834026:AAHreZSHXg1yEs6gmBYTGZ3YnE5BIcwhgsU'
---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+import config
+import random
+
+
+
+bot = telebot.TeleBot(config.telegram_token)
+
+chislo = random.randint(1, 100)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "Привет, я загадал число от 1 до 100. Угадай его.")
+    else:
+        if int(message.text) == chislo:
+            bot.send_message(message.from_user.id, "Вы угадали!!!")
+
+        if int(message.text) > chislo:
+            bot.send_message(message.from_user.id, "Нет это не " + message.text + ". А число которое я загадал меньше.")
+        if int(message.text) < chislo:
+            bot.send_message(message.from_user.id, "Нет это не " + message.text + ". А число которое я загадал больше.")
+
+
+bot.polling()
